d1b69d4306b633468c97aa79ee7e860a19ca4c91
Sergey Kovalenko <seryoga.engineering@gmail.com>
2023-02-02 11:36:30 +0200

    Add installation script with recommended settings

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 This script provide an advanced system cleaning for Linux.
 
 ### Installation
-Default installation directory - $(xdg-user-dir HOME). To change the installation directory provide the argument for `install.sh`.
+Default installation directory - $(xdg-user-dir HOME). To change the installation directory provide the argument for the installation script.
 
 ### Uninstallation
 Run `uninstall.sh` with or without arguments depends on your installation directory.
 
-For the complete functionality install Bleachbit (and Deborphan for Debian/Ubuntu).
+For the complete functionality Bleachbit (and Deborphan for Debian/Ubuntu) are needed. You can install them with the recommended settings with `install_with_recommended_settings.sh` script.
+Once installed, Bleachbit will open to configure user settings.
---
+++ install_with_recommended_settings.sh
@@ -0,0 +1,119 @@
+#!/bin/bash -e
+
+##**************************************************************************
+## MIT License
+##
+## Copyright (C) Sergey Kovalenko <seryoga.engineering@gmail.com>
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+##**************************************************************************
+
+if which apt >/dev/null 2>&1; then
+    echo "Debian/Ubuntu system found"
+    sudo apt install bleachbit deborphan -y
+elif which pacman >/dev/null 2>&1; then
+    echo "Arch Linux system found"
+    sudo pacman -S bleachbit --noconfirm
+else
+    echo "Not supported system. Please install bleachbit manually"
+fi
+
+sudo rm -r /root/.config/bleachbit
+sudo mkdir /root/.config/bleachbit
+cat << 'EOF' | sudo tee /root/.config/bleachbit/bleachbit.ini
+[bleachbit]
+auto_hide = True
+check_beta = False
+check_online_updates = True
+dark_mode = True
+debug = False
+delete_confirmation = True
+exit_done = False
+remember_geometry = True
+shred = False
+units_iec = False
+window_fullscreen = False
+window_maximized = False
+first_start = True
+
+[hashpath]
+
+[list/shred_drives]
+0 = /root/.cache
+1 = /tmp
+
+[preserve_languages]
+en = True
+ru = True
+
+[tree]
+bash = True
+bash.history = True
+journald = True
+journald.clean = True
+system.clipboard = True
+system = True
+system.trash = True
+system.tmp = True
+system.cache = True
+system.desktop_entry = True
+system.recent_documents = True
+system.rotated_logs = True
+
+EOF
+
+if [[ -z $1 ]]; then
+    INSTALL_DIR=~
+else
+    INSTALL_DIR="${1}"
+fi
+
+echo "Installing to ${INSTALL_DIR}"
+
+[[ -f ${INSTALL_DIR}/system-clean.sh ]] && rm ${INSTALL_DIR}/system-clean.sh;
+cp system-clean.sh ${INSTALL_DIR}
+chmod +x ${INSTALL_DIR}/system-clean.sh
+
+cat << 'EOF' | sudo tee /usr/share/applications/system-clean.desktop
+[Desktop Entry]
+Version=1.0
+Encoding=UTF-8
+Type=Application
+Name=System Cleanup
+Name[ru]=Очистка системы
+Comment=System Cleanup
+Comment[ru]=Очистка системы
+GenericName=System Cleanup
+GenericName[ru]=Очистка системы
+Exec=~/system-clean.sh
+Icon=bleachbit
+Categories=GTK;System;
+Keywords=clean;performances;free;privacy;
+StartupNotify=true
+Terminal=true
+EOF
+
+sudo sed -i "s|~|${INSTALL_DIR}|g" /usr/share/applications/system-clean.desktop
+cp /usr/share/applications/system-clean.desktop "$(xdg-user-dir DESKTOP)"
+chmod +x "$(xdg-user-dir DESKTOP)/system-clean.desktop"
+
+if which bleachbit >/dev/null 2>&1; then
+    echo "Setup user defined options"
+    bleachbit
+fi
